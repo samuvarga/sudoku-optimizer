3e6aec335cbf7ee2e0f52ec0d2e7d25d3358b764
Varga Sámuel <samuvarga24@gmail.com>
2025-04-02 12:45:09 +0200

    Solved in X generations

--- soduko_solver.py
+++ soduko_solver.py
@@ -37,7 +37,7 @@ class SudokuGeneticSolver:
                 solution_index = fitness_scores.index(0)
                 solutions.append(population[solution_index])
                 self.visualize_solution(solutions)
-                return population[solution_index]
+                return population[solution_index], generation + 1  # Visszaadjuk a generáció számát
 
             new_population = self.create_new_generation(population, fitness_scores)
             population = new_population
@@ -47,7 +47,7 @@ class SudokuGeneticSolver:
             solutions.append(population[best_index])
 
         self.visualize_solution(solutions)
-        return None
+        return None, None  # Ha nincs megoldás, None-t adunk vissza
 
     def visualize_solution(self, solutions):
         fig, ax = plt.subplots()
@@ -159,22 +159,23 @@ class SudokuGeneticSolver:
 
 if __name__ == "__main__":
     grid = [
-        [5, 3, 4, 6, 7, 0, 0, 1, 0],
-        [6, 7, 2, 1, 9, 0, 0, 4, 0],
-        [1, 9, 8, 3, 4, 0, 0, 6, 0],
-        [8, 5, 9, 7, 6, 0, 0, 2, 0],
-        [4, 2, 6, 8, 5, 0, 0, 9, 0],
-        [7, 1, 3, 9, 2, 0, 0, 5, 0],
-        [9, 6, 1, 5, 3, 0, 0, 8, 0],
-        [2, 8, 7, 4, 1, 0, 0, 3, 0],
-        [3, 4, 5, 2, 8, 0, 0, 7, 0]
+        [0, 3, 4, 6, 7, 8, 9, 1, 0],
+        [6, 0, 2, 1, 0, 5, 3, 0, 8],
+        [1, 9, 0, 3, 4, 2, 0, 6, 7],
+        [8, 5, 9, 0, 6, 0, 4, 2, 3],
+        [0, 2, 0, 8, 0, 3, 0, 9, 0],
+        [7, 1, 3, 0, 2, 0, 8, 5, 6],
+        [9, 6, 0, 5, 0, 7, 0, 8, 4],
+        [2, 0, 7, 4, 1, 9, 6, 0, 5],
+        [0, 4, 5, 2, 0, 6, 1, 7, 0]
     ]
 
     solver = SudokuGeneticSolver(grid)
-    solution = solver.solve_with_visualization()
+    solution, generation = solver.solve_with_visualization()
 
     if solution is not None:
         print("Sudoku solved!")
         print(np.array(solution))
+        print(f"Solved in {generation} generations.")
     else:
         print("No solution found.")
